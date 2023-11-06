7d9ce414609bc1996035a9f6d13b25c996115c5f
zkq12138 <144882789+zkq12138@users.noreply.github.com>
2023-11-06 21:41:06 +0800

    Add files via upload

--- log_reg.py
+++ log_reg.py
@@ -1,7 +1,9 @@
 import streamlit as st
+import pandas as pd
 
 
 def log_reg():
+    users = pd.read_table('users.txt', sep=' ', header=None)
     st.markdown('''<h2 style='color: blue;text-align: center;'>⌚Luxury Watches</h2>''', unsafe_allow_html=True)
     st.markdown('''
         <p>Explore the world of luxury timepieces with this comprehensive Luxury Watches Dataset. This collection offers a rich source of data on high-end wristwatches, featuring details on brands, models, materials, complications, and more. Whether you're a horology enthusiast, a data scientist, or a watch industry professional, this dataset provides valuable insights into the fascinating realm of luxury watchmaking.</p>
@@ -16,5 +18,12 @@ def log_reg():
     st.text_input(label="username", key="username", placeholder="Please enter your username")
     st.text_input(label="password", key="password", type='password', placeholder="Please enter the corresponding password")
     if st.button("Login", type="primary"):
-        st.session_state["user"] = st.session_state["username"]
-        st.experimental_rerun()
+        if st.session_state["username"] not in users[0].tolist():
+            st.error('No such user')
+            st.stop()
+        elif st.session_state["username"] in users[0].tolist() and st.session_state["password"] == users.set_index(0).loc[st.session_state["username"]][1]:
+            st.session_state["user"] = st.session_state["username"]
+            st.experimental_rerun()
+        else:
+            st.error('Login parameters are incorrect, please try again')
+            st.stop()
